14341e93a8f54210dfc9dc0027f0178455424c80
peh.aalmeida <devpedroalmeida@hotmail.com>
2025-06-16 12:08:31 -0300

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
-  base: '/meu-portfolio/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
